8b7c32002d6be5491fd309856edeee2886c0014e
IgorVasilyev <v-i-v-igor@yandex.ru>
2021-01-08 14:14:54 +0300

    Additional HomeWork

---
+++ Additional/AverageNumber.java
@@ -0,0 +1,30 @@
+package Additional;
+
+import java.util.Scanner;
+
+public class AverageNumber {
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число а");
+        a = scanner.nextInt();
+        System.out.println("Введите число b");
+        b = scanner.nextInt();
+        System.out.println("Введите число c");
+        c = scanner.nextInt();
+
+        if (a == b || a == c || b == c) {
+            System.out.println("Введены рановзначные значения a = b = c = " + b);
+        } else if (a > b && a < c || a < b && a > c) {
+            System.out.println("Число а " + a);
+        } else if (b > a && b < c || b < a && b > c) {
+            System.out.println("Число b " + b);
+        } else {
+            System.out.println("Число с " + c);
+        }
+
+    }
+
+}
---
+++ Additional/Symbol.java
@@ -0,0 +1,23 @@
+package Additional;
+
+import java.util.Scanner;
+
+public class Symbol {
+
+    public static void main(String[] args) {
+
+        int a;
+        System.out.println("Введите челое число от 0 до 256");
+        Scanner sn = new Scanner(System.in);
+        a = sn.nextInt();
+
+        if ( a < 0 || a > 256 ) {
+            System.out.println("Таблица ASCII включает 256 символов");
+        } else if (a >= 'A' && a <= 'Z' || a >= 'a' && a <= 'z') {
+                System.out.println("Это буква " + (char) a);
+        } else {
+                System.out.print("Это не буква, а символ " + (char) a);
+            }
+
+    }
+}
