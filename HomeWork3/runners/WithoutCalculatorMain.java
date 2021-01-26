b023b6e44b472c27e7c56a9c21ccc94eae5bffcf
Igor Vasilyev <v-i-v-igor@yandex.ru>
2021-01-26 21:27:51 +0300

    HomeWork3.1

--- HomeWork3/runners/WithoutCalculatorMain.java
+++ HomeWork3/runners/WithoutCalculatorMain.java
@@ -3,6 +3,6 @@ package HomeWork3.runners;
 public class WithoutCalculatorMain {
     public static void main(String[] args) {
         double result = 4.1 + 15 * 7 + (double)(28 / 5 * 28 / 5);
-        System.out.printf("result = " + result);
+        System.out.println("result = " + result);
     }
 }
